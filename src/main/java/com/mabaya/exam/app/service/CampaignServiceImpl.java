965dccaf3f1bdb04f515e7285c16c5a3032cfb82
slavaOlshanechky <olshanechky@gmail.com>
2021-01-23 13:24:42 +0200

    Disabled MongoDB settings in the pom.xml and in the CampaignRepository interface Changed endpoints params from @RequestBody to @Pathvariables in class AppController. For this reason changed methods in CampaignService and CampaignServiceImpl

--- src/main/java/com/mabaya/exam/app/service/CampaignServiceImpl.java
+++ src/main/java/com/mabaya/exam/app/service/CampaignServiceImpl.java
@@ -1,29 +1,34 @@
 package com.mabaya.exam.app.service;
 
-import com.mabaya.exam.app.dao.CampaignRepository;
 import com.mabaya.exam.app.dto.AddCampaignDto;
 import com.mabaya.exam.app.dto.CreatedCampaignResponseDto;
 import lombok.Builder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Builder
 @Service
 public class CampaignServiceImpl implements CampaignService {
-    @Autowired
-    CampaignRepository campaignRepository;
+//    @Autowired
+//    CampaignRepository campaignRepository;
+
+//    @Override
+//    public CreatedCampaignResponseDto addNewCampaign(AddCampaignDto newCampaign) {
+//        //Campaign campaign = CampaignRepository.findByOwnerAndEventId(sessionLogin, eventId);
+////if(!campaignRepository.findByName(name));
+////        return CreatedCampaignResponseDto.builder() //it does not work
+////                .name()
+////                .startDate()
+////                .category()
+////                .bid()
+////                .build();
+//
+//        return null;
+//    }
 
     @Override
-    public CreatedCampaignResponseDto addNewCampaign(AddCampaignDto newCampaign) {
-        //Campaign campaign = CampaignRepository.findByOwnerAndEventId(sessionLogin, eventId);
-//if(!campaignRepository.findByName(name));
-//        return CreatedCampaignResponseDto.builder() //it does not work
-//                .name()
-//                .startDate()
-//                .category()
-//                .bid()
-//                .build();
-
+    public CreatedCampaignResponseDto addNewCampaign(String name, LocalDate startDate, String category, Double bid) {
         return null;
     }
 }
